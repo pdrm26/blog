3ba71cf2aa33e1b235c6e81d1af1993c49c62e75
pedram <peeeedii@gmail.com>
2024-10-25 18:46:14 +0330

    refactor: alter from function view to class base view for post_list

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.published.all()
-
-    # Pagination with 3 posts per page
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page_number is not an integer get the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # if page_number is out of range get last page of results
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request, "blog/post/list.html", {"posts": posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, post):
