f116fba875f30f093532e4a3f517b3ca269d69d7
pedram <peeeedii@gmail.com>
2024-10-25 14:59:45 +0330

    feat: unique each post with slug and the date

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.utils import timezone
 from django.db import models
 
@@ -17,7 +18,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "PUBLISHED"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
