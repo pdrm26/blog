8b3b938eaac4d6429148ecdbb5eacc2464edace2
pedram <peeeedii@gmail.com>
2024-10-31 22:25:55 +0330

    feat: adding stemming for the stop words and add ranking to search results

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,11 @@ from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank
+)
 
 from .models import Post
 
@@ -124,8 +128,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = (
-                Post.published.annotate(search=SearchVector('title', 'body'))
-            ).filter(search=query)
+                Post.published.annotate(
+                    search=search_vector, rank=SearchRank(search_vector, search_query))
+            ).filter(search=query).order_by('-rank')
 
     return render(request, "blog/post/search.html", {"form": form, "query": query, "results": results})
