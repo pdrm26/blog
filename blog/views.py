be147d0dacdbb527798f17cf73a4ba84e21d9001
pedram <peeeedii@gmail.com>
2024-10-31 22:18:58 +0330

    feat: add search view

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 from .models import Post
 
@@ -59,10 +60,9 @@ def post_detail(request, year, month, day, post):
 
     post_tags_ids = post.tags.values_list('id', flat=True)
     similar_posts = Post.published.filter(
-    tags__in=post_tags_ids).exclude(id=post.id)
+        tags__in=post_tags_ids).exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count(
-    "tags")).order_by("-same_tags", "-publish")[:4]
-
+        "tags")).order_by("-same_tags", "-publish")[:4]
 
     return render(request, "blog/post/detail.html", {"post": post, "comments": comments, "form": form, "similar_posts": similar_posts})
 
@@ -113,3 +113,19 @@ def post_comment(request, post_id):
         # Save the comment to the database
         comment.save()
     return render(request, "blog/post/comment.html", {"post": post, "form": form, "comment": comment})
+
+
+def post_search(request):
+    form = SearchForm
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(search=SearchVector('title', 'body'))
+            ).filter(search=query)
+
+    return render(request, "blog/post/search.html", {"form": form, "query": query, "results": results})
